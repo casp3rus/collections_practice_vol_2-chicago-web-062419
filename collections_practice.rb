0be04362983f3e360fcbd3cfe0a30f3dde82e338
Adrian Popan <adrian.popan@gmail.com>
2019-06-24 03:45:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,34 +33,34 @@ def first_wa(array)
   first_wa
 end
 
-# def find_cool(array)
-#   container = []
-#   array.each do |element|
-#     container << element if element[:temperature] == "cool"
-#   end
-#   container
-# end
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
 
-# def count_elements(array)
-#   array.each do |original_hash|
-#     original_hash[:count] = 0
-#     name = original_hash[:name]
-#     array.each do |hash|
-#       if hash[:name] == name
-#         original_hash[:count] += 1
-#       end
-#     end
-#   end.uniq
-# end
+def merge_data(arr1, arr2)
+  arr2[0].map do |name, prop_hash|
+    new_prop_hash = {}
+    arr1.each do |new_attr_hash|
+      if new_attr_hash[:first_name] == name
+        new_prop_hash = prop_hash.merge(new_attr_hash)
+      end
+    end
+    new_prop_hash
+  end
+end
 
-# def merge_data(arr1, arr2)
-#   arr2[0].map do |name, prop_hash|
-#     new_prop_hash = {}
-#     arr1.each do |new_attr_hash|
-#       if new_attr_hash[:first_name] == name
-#         new_prop_hash = prop_hash.merge(new_attr_hash)
-#       end
-#     end
-#     new_prop_hash
-#   end
-# end
+def find_cool(array)
+  container = []
+  array.each do |element|
+    container << element if element[:temperature] == "cool"
+  end
+  container
+end
